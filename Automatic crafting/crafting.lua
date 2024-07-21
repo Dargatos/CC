9a7ef917ce65374e137fb495f64192b635fef5c4
Dargatos <miomenn@gmail.comm>
2024-07-21 16:16:52 +0200

    clean up

--- Automatic crafting/crafting.lua
+++ Automatic crafting/crafting.lua
@@ -1,12 +1,22 @@
 local me = peripheral.find("meBridge")
 local monitor = peripheral.find("monitor")
 
+
+
 meItemList = {
     [1] = {name = "SteelIngots", id = "mekanism:ingot_steel", amount = 5000},
     [2] = {name = "Atomic Alloy", id = "mekanism:alloy_atomic", amount = 1000},
     [3] = {name = "inductionCell", id = "mekanism:ultimate_induction_cell",amount = 5},
     [4] = {name = "Spruce Planks", id = "minecraft:spruce_planks", amount = 1024}
 }
+
+function addToList(name,id,amount)
+    if not amount then
+        amount = 5000
+    end
+    newitem = {name = name,id = id,amount = amount}
+    meItemList.insert(newitem)
+end
 
 local function checkItems(itemID)
     local meItem = me.getItem({name = itemID})
@@ -30,45 +40,58 @@ local function checkItems(itemID)
         isCrafting = false
     end
 
-    return itemAmount, isCrafting
+    local craftable = me.isItemCraftable({name = itemID})
+
+    return itemAmount, isCrafting , craftable
 end
 
 local function craftItems(ItemID, value)
     ItemToCraft = {name = ItemID,count = value}
     crafting, err = me.craftItem(ItemToCraft)
+    print (err)
     return crafting, err
 end
 
 local function calcToCraft(stocked, desired)
     local missing = desired - stocked
-    print(missing)
     return missing
+end
+
+local function getinfo()
+    for i, item in ipairs(meItemList)do
+
+        local name = item.name
+        local id = item.id
+        local amount = item.amount
+        local craftable
+        local toCraft
+
+        stockedItems, isCrafting, craftable = checkItems(id)
+        toCraft = calcToCraft(stockedItems,amount)
+        
+        if not craftable then 
+            printError(name .. " is not Craftable")
+        elseif not isCrafting and toCraft > 0 then
+            -- Start a crating Job
+            
+            getscrafted, craftingError = craftItems(id,toCraft)
+            
+            if getscrafted then
+                print("Started crafting some tasty " .. name .. " " .. toCraft .. " pieces")
+            else
+                printError("Crafting Error: ".. name .. " " .. craftingError)
+            end
+        end
+        
+    end
+
 end
 
 local function main()
 
     -- Main loop of cheking items and crafting if necessary
     while true do
-        for i, item in ipairs(meItemList)do
-            local name = item.name
-            local id = item.id
-            local amount = item.amount
-            print(name,id,amount)
-            stockedItems, isCrafting, err = checkItems(id)
-
-            local toCraft = calcToCraft(stockedItems,amount)
-
-            -- Start a crating Job
-            if not isCrafting and toCraft > 0 then
-                getscrafted, craftingError = craftItems(id,toCraft)
-                if getscrafted then
-                    print("Started crafting some tasty " .. name .. " " .. toCraft .. " pieces")
-                else
-                    printError("Crafting Error: ".. name .. " " .. craftingError)
-                end
-            end
-                
-        end
+        getinfo()
         sleep(5)
     end
 end
