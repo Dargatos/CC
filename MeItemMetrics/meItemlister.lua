7bf5720d352ff365aa72bdd51f0bd31b2b2c8a96
Dargatos <miomenn@gmail.com>
2024-07-30 22:41:26 +0200

    updated meItemLister tho doesnt work right for now atleast(But way cleaner and more usable modularer lol)

--- MeItemMetrics/meItemlister.lua
+++ MeItemMetrics/meItemlister.lua
@@ -1,34 +1,78 @@
 -- Adding peripherals
 local monitor = peripheral.find("monitor")
 local me = peripheral.find("meBridge")
+local screen = "Stats"
 
 -- Declaring tables
-local list = {"minecraft:wheat","minecraft:cobblestone","mekanism:dust_sulfur", "minecraft:diamond","minecraft:iron_ingot","minecraft:redstone","minecraft:gold_ingot","malum:wicked_spirit","malum:arcane_spirit","malum:aerial_spirit","malum:earthen_spirit","malum:eldritch_spirit","mekanism:sodium"}
-
--- Build new table which stores the data (amount) of items in "list"
-local function buildTable(list)
-    local newList = {}
-    for i, name in ipairs(list) do
-        local item = {name = name, children = {}}
-        table.insert(newList, item)
+local chosenItems = {
+    "minecraft:wheat",
+    "minecraft:cobblestone",
+    "mekanism:dust_sulfur",
+    "minecraft:diamond",
+    "minecraft:iron_ingot", 
+    "minecraft:redstone",
+    "minecraft:gold_ingot",
+    "malum:wicked_spirit",
+    "malum:arcane_spirit",
+    "malum:aerial_spirit",
+    "malum:earthen_spirit",
+    "malum:eldritch_spirit",
+    "mekanism:sodium"
+}
+meStats = {}
+maxData = 700
+local function savetData(meitem)
+    -- Checks if there is already a table
+    local noTable = true
+    if #meStats == 0 then 
+        noTable = true 
     end
-    return newList
-end
-
-local table_list = buildTable(list)
-
-local function add_amount_values(item_name, child_value)
-    for i, item in ipairs(table_list) do
-        if item.name == item_name then
-            local count = #table_list[1].children
-            table.insert(item.children, child_value)
-            --if count > 700 then
-            --    table.remove(item.children, 1)
-            --end
-            return true
+    for i, item in pairs(meStats) do
+        if item.name == meitem.name then
+            noTable = false
+            break
         end
     end
-    return false
+
+    -- if there is no table create and stores the new one
+    if noTable == true then
+
+        table.insert(meStats,{name = meitem.name,stats = {}})
+    end
+
+    -- Saves the data in the table
+    for i, entry  in pairs(meStats) do
+        if entry.name == meitem.name then
+            local amount = meitem.amount or 0
+            table.insert(entry.stats,1 ,amount)
+        end
+    end    
+end
+
+local function storeMeStats()
+    while true do
+        local melist = {}
+
+        -- Sometimes Me doesnt return any data so thats for the case it does that
+        while true do
+            melist = me.listItems()
+            if melist then
+                break
+            end
+            printError("me didnt return values")
+            sleep(1)
+        end
+
+        for i, item in pairs(melist) do
+            savetData(item)
+        end
+
+        if #meStats > maxData then
+            table.remove(meStats,1)
+        end
+        print("Stats Saved")
+        sleep(1)
+    end
 end
 
 local function read_system_values()
@@ -44,13 +88,10 @@ local function read_system_values()
         sleep(1)
     end
 
-    print(items)
-
     -- Searching for items in "list" and storing value in array
     for _, item_name in ipairs(list) do
         for _, item in pairs(items) do
             if item.name == item_name then
-                --print ("Amount : ",item.amount)
                 add_amount_values(item_name, item.amount)
                 
             end
@@ -61,53 +102,10 @@ local function read_system_values()
     return true
 end
 
-
-
-local function get_nth_child_value(item_name, n)
-    
-    for i, item in ipairs(table_list) do
-        if item.name == item_name then
-            if #item.children < n then
-                return 0
-            end
-            return item.children[n] or 0
-        end
-    end
-    return 0
-end
-
-local function clac_diff(name)
-    local count = #table_list[1].children  -- Total number of data points
-    local last_value = get_nth_child_value(name, count)  -- Get the last recorded value
-
-    local ten_sec, sixty_sec, sixhundred_sec
-
-    if count >= 10 then
-        local ten_sec_value = get_nth_child_value(name, count - 10)  -- Value 10 positions ago
-        ten_sec = (last_value - ten_sec_value) / 10 
-        ten_sec = string.format("%.2f/s", ten_sec)  -- Round to 2 decimal places
-    else
-        ten_sec = "Wait"
-    end
-
-    if count >= 60 then
-        local sixty_sec_value = get_nth_child_value(name, count - 60)  -- Value 60 positions ago
-        sixty_sec = (last_value - sixty_sec_value)
-        sixty_sec = string.format("%.0f/m", sixty_sec)  -- Round to 2 decimal places
-    else
-        sixty_sec = "Wait"
-    end
-
-    if count >= 600 then
-        local sixhundred_sec_value = get_nth_child_value(name, count - 600)  -- Value 600 positions ago
-        sixhundred_sec = (last_value - sixhundred_sec_value) / 600 * 60 * 60
-        sixhundred_sec = string.format("%.2f/h", sixhundred_sec)  -- Round to 2 decimal places
-    else
-        sixhundred_sec = "Wait"
-    end
-
-    return {ten_sec, sixty_sec, sixhundred_sec}
-end
+-- Im bad at remembering so for my brain some copys fo string functions
+-- sixhundred_sec = string.format("%.2f/h", sixhundred_sec)  -- Round to 2 decimal places
+-- sixty_sec = string.format("%.0f/m", sixty_sec)  -- Round to 2 decimal places
+-- ten_sec = string.format("%.2f/s", ten_sec)  -- Round to 2 decimal places
 
 local function formatWithDots(number)
     -- Convert the number to a string
@@ -117,7 +115,7 @@ local function formatWithDots(number)
     local reversed = string.reverse(numStr)
 
     -- Insert dots every three characters
-    local withDots = reversed:gsub("(%d%d%d)", "%1 ")
+    local withDots = reversed:gsub("(%d%d%d)", "%1.")
 
     -- Reverse the string back and remove any leading dot
     withDots = string.reverse(withDots):gsub("^% ", "")
@@ -175,14 +173,125 @@ local function display_on_monitor()
     end
 end
 
+local function getIndexofItem(choosen)
+    for i, meItem in pairs(meStats) do
+        if choosen == meItem.name then
+            return i
+        end
+    end
+    return false
+end
 
+local function drawItemData(x,y,textcolor,switch,bg1,bg2,displayIncrease,choosenitem,storedOffst,increaseOffset)
+    local storedOffset = storedOffset or 19
+    local increaseOffset = increaseOffset or 11
+    local increaseOffset = increaseOffset + storedOffset
+    monitor.setCursorPos(x,y)
+    monitor.setTextColor(textcolor)
+
+    if switch == true then
+        -- Swithces Bg color every line
+        if (y % 2 == 0) then
+            monitor.setBackgroundColor(bg1)
+        else
+            monitor.setBackgroundColor(bg2)
+        end
+    end
+    
+    -- Draw name
+    local _, _, itemName = string.find(choosenitem, ":(.*)")
+    monitor.write(itemName)
+    monitor.write(string.rep(" ",width))
+
+
+    -- Draw Stored amount
+    indexofItem = getIndexofItem(choosenitem)
+
+    monitor.setCursorPos(x + storedOffset,y)
+    if not indexofItem  == false then
+        monitor.write(meStats[indexofItem].stats[1])
+        
+    else
+        monitor.write("0")
+    end
+
+    --  ______________________________________________________________________________
+    --  Add a varable for how many increae should be shown also the               |
+    --  varivale of how strong they should me like not every 10 60 600 seconds    |
+    --  but 60 600 3600 or similar                                                V
+
+    -- Draw increase
+    monitor.setCursorPos(x + increaseOffset, y)
+
+    if not indexofItem  == false then
+        if displayIncrease then
+            if meStats[indexofItem].stats[10] then
+                increase = (meStats[indexofItem].stats[1] - meStats[indexofItem].stats[10]) / 10
+                monitor.write(increase)
+            else
+                monitor.write("Wait")
+            end
+            monitor.setCursorPos(x + increaseOffset + 7, y)
+            if meStats[indexofItem].stats[60] then
+                increase = (meStats[indexofItem].stats[1] - meStats[indexofItem].stats[60]) / 60
+                monitor.write(increase)
+            else
+                monitor.write("Wait")
+            end
+            monitor.setCursorPos(x + increaseOffset + 14, y)
+            if meStats[indexofItem].stats[600] then
+                increase = (meStats[indexofItem].stats[1] - meStats[indexofItem].stats[600]) / 600
+                monitor.write(increase)
+            else
+                monitor.write("Wait")
+            end
+        end
+    else
+        monitor.write("Wait   Wait   Wait")
+    end
+end
+
+local function getMonitorSize()
+    monX , monY = monitor.getSize()
+    return monX, monY
+end
+
+local function tracker()
+    storeMeStats()
+end
+
+local function touchHandler()
+    local event, side, x, y = os.pullEvent("monitor_touch")
+end
+
+local function diplayStats()
+    while true do
+        width, height = getMonitorSize()
+        monitor.setBackgroundColor(colors.black)
+        monitor.setTextColor(colors.white)
+        monitor.setCursorPos(width/2 - 14,1)
+        monitor.write("Items stats of the Me System")
+        monitor.setPaletteColor(colors.black,0x000000)
+        monitor.setPaletteColor(colors.gray,0x0b0b0b)
+        local lastData = meStats[#meStats]
+
+        for i, name in pairs(chosenItems) do
+            drawItemData(1,3 + i,colors.white,true,colors.black,colors.gray,true,name)
+        end
+        sleep(1) -- Start the function every second instead 
+                --of after every sencond to be more percise and not hardware thingy
+    end
+
+end
+
+local function startMonitor()
+    screen = "Stats"
+    monitor.clear()
+end
 
 local function main()
-    while true do
-        read_system_values()
-        display_on_monitor()
-        sleep(.6)
-    end
+    startMonitor()
+    parallel.waitForAny(storeMeStats, diplayStats)
 end
 
 main()
